24e84f88add046025534d4db4201279889ea4447
Meonardo <sisxme@gmail.com>
2022-10-22 20:29:41 +0800

    Update build libwebrtc script.

--- deps/libwebrtc/scripts/build-libwebrtc.bat
+++ deps/libwebrtc/scripts/build-libwebrtc.bat
@@ -1,16 +1,23 @@
-SET OUT_PATH=%~1
+SET ROOT_DIR=%1
+SET SCHEME=%~2
+SET OUTPUT_PATH=%~3
+
+if "%SCHEME%" == "Debug" (
+    SET SCHEME="debug" 
+) else ( 
+    SET SCHEME="release"
+)
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
 
-set root=D:\File\WebRTC\LibWebRTC\src\libwebrtc\scripts
-CD /D %root%
+cd /D %ROOT_DIR%
 
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
 set GYP_MSVS_VERSION=2019
 set GYP_GENERATORS=ninja,msvs-ninja
 set GYP_MSVS_OVERRIDE_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional
 
-set VAR_1=--scheme release
-set VAR_2=--output_path %OUT_PATH%
+set VAR_1=--scheme %SCHEME%
+set VAR_2=--output_path %OUTPUT_PATH%
 
 python build-libwebrtc-win.py %VAR_1% %VAR_2%
