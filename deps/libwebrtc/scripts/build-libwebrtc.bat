c7260d391dd6d402442da42bc96d2e5ceaffcfaf
Meonardo <sisxme@gmail.com>
2023-01-09 11:31:39 +0800

    Update building libwebrtc script.

--- deps/libwebrtc/scripts/build-libwebrtc.bat
+++ deps/libwebrtc/scripts/build-libwebrtc.bat
@@ -8,14 +8,14 @@ if "%SCHEME%" == "Debug" (
     SET SCHEME="release"
 )
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
 
 cd /D %ROOT_DIR%
 
 set DEPOT_TOOLS_WIN_TOOLCHAIN=0
-set GYP_MSVS_VERSION=2019
+set GYP_MSVS_VERSION=2022
 set GYP_GENERATORS=ninja,msvs-ninja
-set GYP_MSVS_OVERRIDE_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional
+set GYP_MSVS_OVERRIDE_PATH=C:\Program Files\Microsoft Visual Studio\2022\Professional
 
 set VAR_1=--scheme %SCHEME%
 set VAR_2=--output_path %OUTPUT_PATH%
